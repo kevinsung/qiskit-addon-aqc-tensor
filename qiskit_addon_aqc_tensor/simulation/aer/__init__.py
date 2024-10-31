8665523ff8ef40f8502fc4df05fb17bb11242c8f
Jim Garrison <garrison@ibm.com>
2024-10-31 15:05:12 -0400

    Initial commit, with aer backend and explicit gradient logic
    Much of this code derives from the original research code,
    described at https://arxiv.org/abs/2301.08609
    
    Co-authored-by: Kate Marshall <kate.marshall@ibm.com>
    Co-authored-by: Niall Robertson <niall.robertson@ibm.com>
    Co-authored-by: ALBERT AKHRIEV <albert_akhriev@ie.ibm.com>

---
+++ qiskit_addon_aqc_tensor/simulation/aer/__init__.py
@@ -0,0 +1,28 @@
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2024.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Qiskit Aer MPS simulator as a tensor network backend."""
+
+from .gradient import _preprocess_for_gradient
+from .simulation import QiskitAerSimulationSettings, is_aer_available
+from .state import QiskitAerMPS
+
+# We want the plum-dispatch methods from gradient.py to be available, so we
+# have made an unused import from that file.  But now we must reference that
+# import so the linter does not complain.
+_ = _preprocess_for_gradient
+
+__all__ = [
+    "is_aer_available",
+    "QiskitAerMPS",
+    "QiskitAerSimulationSettings",
+]
